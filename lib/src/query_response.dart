8c08f4bb4108d85e54b91205a2d9c0913430edc6
Alexey Salnikov <cartrege@gmail.com>
2014-11-27 14:34:11 +0300

    query_response add methods

--- lib/src/query_response.dart
+++ lib/src/query_response.dart
@@ -15,4 +15,28 @@ class QueryResponse {
     _decoded = JSON.decode(json);
   }
 
+  bool get hasError {
+    return _decoded.containsKey('error');
+  }
+
+  int get errorCode {
+    return hasError ? _decoded['error']['error_code'] : 0;
+  }
+
+  String get errorMessage {
+    return hasError ? _decoded['error']['error_msg'] : '';
+  }
+
+  Map<String, String> get requestParams {
+    return hasError ? _decoded['error']['request_params'] : '';
+  }
+
+  get data {
+    return hasError ? null : _decoded['response'];
+  }
+
+  String toString() {
+    return _decoded.toString();
+  }
+
 }
